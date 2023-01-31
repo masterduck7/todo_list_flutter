f5f4274b699616a138108fddaded182718cde9f1
Luis Ramirez <luis.ramirez.12@sansano.usm.cl>
2023-01-30 22:19:51 -0300

    feat(app): Add asset images and update splash view

--- lib/app/views/splash.dart
+++ lib/app/views/splash.dart
@@ -5,9 +5,38 @@ class SplashView extends StatelessWidget{
 
   @override
   Widget build( context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World'),
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                'assets/images/Vector.png',
+                width: 141,
+                height: 129,
+              ),
+            ],
+          ),
+          const SizedBox(height: 73),
+          Image.asset(
+            'assets/images/onboarding-image.png',
+            width: 180,
+            height: 168,
+          ),
+          const SizedBox(height: 99),
+          Text('Lista de Tareas',
+              style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                  fontSize: 18, fontWeight: FontWeight.w600)
+          ),
+          const SizedBox(height: 21),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            child: Text('La mejor forma para que no se te olvide nada es anotarlo. Guardar tus tareas y ve completando poco a poco para aumentar tu productividad',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
       ),
     );
   }
