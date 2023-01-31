b194d0b8cdd96518621909f2372c8b1c778d04fe
Luis Ramirez <luis.ramirez.12@sansano.usm.cl>
2023-01-30 22:04:07 -0300

    feat(app): Update theme colors and use fonts loaded

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -7,10 +7,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const primary = Color(0xFF40B7AD);
+    const textColor = Color(0xFF4a4a4a);
+    const backgroundColor = Color(0xFFF5F5F5);
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TODO List',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: primary),
+        scaffoldBackgroundColor: backgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(
+          fontFamily: 'Poppins',
+          bodyColor: textColor,
+          displayColor: textColor,
+        )
       ),
       home: const SplashView(),
     );
