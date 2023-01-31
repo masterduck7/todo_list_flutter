420ad47ceede4035ad49c6cad74b46217d16ce10
Luis Ramirez <luis.ramirez.12@sansano.usm.cl>
2023-01-30 21:52:17 -0300

    feat(app): Create new splash view

---
+++ lib/app/views/splash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget{
+  const SplashView({super.key});
+
+  @override
+  Widget build( context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+
+}
