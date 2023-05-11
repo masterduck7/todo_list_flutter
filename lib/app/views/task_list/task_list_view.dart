224b91252f6166487b449c089e1f8cb6e3d036f4
Luis Ramirez <lp.ramirez.duarte.7@gmail.com>
2023-05-11 19:50:20 +0200

    feat(task_list): Create model task, add new asset for view. Update task list view with dummy data

--- lib/app/views/task_list/task_list_view.dart
+++ lib/app/views/task_list/task_list_view.dart
@@ -1,30 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_flutter/app/model/task.dart';
+import 'package:todo_list_flutter/app/views/components/text_title.dart';
+import 'package:todo_list_flutter/app/views/components/vector.dart';
 
-class TaskListView extends StatefulWidget {
+class TaskListView extends StatelessWidget {
   const TaskListView({Key? key}) : super(key: key);
-
-  @override
-  State<TaskListView> createState() => _TaskListViewState();
-}
-
-class _TaskListViewState extends State<TaskListView> {
-  int count = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Task List"),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            color: Theme.of(context).colorScheme.primary,
+            child: Column(
+              children: [
+                Row(children: const [Vector()]),
+                Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/tasks-list-image.png',
+                      width: 120,
+                      height: 120,
+                    ),
+                    const SizedBox(height: 16),
+                    const TextTitle(text: 'Completa tus tareas', color: Colors.white),
+                    const SizedBox(height: 32),
+                  ],
+                )
+              ],
+            ),
+          ),
+          const Padding(
+              padding: EdgeInsets.only(left: 30, right: 30, top: 25),
+              child: TextTitle(text: 'Tareas')
+          ),
+          Expanded(
+            child: ListView.separated(
+                itemCount: taskList.length,
+                separatorBuilder: (a,b) => const SizedBox(height: 16),
+                itemBuilder: (_, index) => Text(taskList[index].title),
+            ),
+          )
+        ],
       ),
-      body: Center(child: Text("Actual value: $count")),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              count++;
-            });
-          },
-          child: const Icon(Icons.add)
-        ),
-      );
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add, size: 50),
+      ),
+    );
   }
 }
+
+final taskList = [
+  Task("Task 1"),
+  Task("Task 2"),
+  Task("Task 3"),
+];
